41abcfc009e9fbc30af346c1822347c0cef9bfe8
Luca-Milioli <317076@studenti.unimore.it>
2025-07-02 10:03:44 +0200

    round factory

--- lo_sapevi_che/scripts/main.gd
+++ lo_sapevi_che/scripts/main.gd
@@ -1,22 +1,67 @@
 extends Node
 
-var round_scene = preload("res://scenes/round.tscn")
+
 
 func _on_menu_play_pressed() -> void:
 	print("scimpanzini bananini")
-	round_factory(1)
+	const ROUNDS = 5
+	for round in range(ROUNDS):
+		round_factory(round)
 
 func round_factory(round_number):
-	match round_number:
-		1:
-			print("creating round 1")
-		2:
-			print("creating round 2")
-		3:
-			print("creating round 3")
-		4:
-			print("creating round 4")
-		5:
-			print("creating round 5")
-		_:
-			print("error")
+	
+	var lambda_question = func() -> Question:
+		var question : Question = null
+		match round_number:
+			1:
+				return question.new()
+			2:
+				return question.new()
+			3:
+				return question.new()
+			4:
+				return question.new()
+			5:
+				return question.new()
+			_:
+				return null
+	
+	var lambda_answers = func() -> Array[Answer]:
+			var answers: Array[Answer]
+		
+			var correct_answer = func(text: String) -> void:
+				answers.append(RightAnswer.new().inizialize(text))
+				
+			var wrong_answers = func(texts: Array[String]) -> void:
+				for text in texts:
+					answers.append(WrongAnswer.new().inizialize(text))
+			
+			var make_answer = func() -> void:
+				match round_number:
+					1:
+						correct_answer.call("a")
+						wrong_answers.call("b")
+					2:
+						correct_answer.call("a")
+						wrong_answers.call("b")
+					3:
+						correct_answer.call("a")
+						wrong_answers.call("b")
+					4:
+						correct_answer.call("a")
+						wrong_answers.call("b")
+					5:
+						correct_answer.call("a")
+						wrong_answers.call("b")
+					_:
+						return
+			
+			make_answer.call()
+			return answers
+	
+	var round = preload("res://scenes/round.tscn").instantiate()
+	round.new()
+	round.setup(lambda_question.call(round_number), lambda_answers.call(round_number))
+	Round.round_count += 1
+	return round
+	
--- lo_sapevi_che/scripts/round.gd
+++ lo_sapevi_che/scripts/round.gd
@@ -1,13 +1,16 @@
 extends Control
+class_name Round
 
 static var round_count: int
+var question: Question
+var answers: Array[Answer]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func setup(question: Question, answers: Array[Answer]):
-	Round.round_count += 1
+	
 	_cleaner()
 
 func _cleaner():
