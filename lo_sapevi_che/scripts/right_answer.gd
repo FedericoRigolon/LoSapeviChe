79e9e11f7f6d564f49c50a47e96310b5fa26b1f9
FedericoRigolon <318199@studenti.unimore.it>
2025-07-02 10:36:32 +0200

    Sound

--- lo_sapevi_che/scripts/right_answer.gd
+++ lo_sapevi_che/scripts/right_answer.gd
@@ -4,8 +4,8 @@ class_name RightAnswer
 
 const HIGHLIGHT_COLOR = Color(0, 1, 0, 1) # green
 
-func inizialize(text):
-	super.inizialize(text)
+func setup(text):
+	super.setup(text)
 	_set_sound("res://art/sounds/right.mp3")
 
 func _highlight() -> void:
--- lo_sapevi_che/scripts/wrong_answer.gd
+++ lo_sapevi_che/scripts/wrong_answer.gd
@@ -4,8 +4,8 @@ class_name WrongAnswer
 
 const HIGHLIGHT_COLOR = Color(1, 0, 0, 1) # red
 
-func inizialize(text):
-	super.inizialize(text)
+func setup(text):
+	super.setup(text)
 	_set_sound("res://art/sounds/wrong.mp3")
 
 func _highlight() -> void:
